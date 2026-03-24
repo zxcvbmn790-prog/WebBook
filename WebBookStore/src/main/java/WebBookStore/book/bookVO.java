83486519537cab947189dddea2967ef5f3cb343e
seoil <seoil@host.docker.internal>
2026-03-24 19:05:35 +0900

    0

--- WebBookStore/src/main/java/WebBookStore/book/bookVO.java
+++ WebBookStore/src/main/java/WebBookStore/book/bookVO.java
@@ -1,5 +1,18 @@
 package WebBookStore.book;
 
-public class bookVO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+// Lombok 사용
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class bookVO {
+    private int id;             // 책 ID
+    private String title;       // 책 제목
+    private String author;      // 저자
+    private String publisher;   // 출판사
+    private double price;       // 가격
+    private int stock;          // 재고
 }
