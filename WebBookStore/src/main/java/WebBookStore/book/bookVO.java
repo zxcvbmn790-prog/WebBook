bf5afe75d3ac9779cfd3efd31c19397acb3197e4
summitpsy2 <summitpsy@gmail.com>
2026-03-25 17:24:46 +0900

    사용자 전체출력, 선택출력
    Refactored bookVO class to BookVO with new fields and Lombok annotations.

--- WebBookStore/src/main/java/WebBookStore/book/bookVO.java
+++ WebBookStore/src/main/java/WebBookStore/book/bookVO.java
@@ -2,17 +2,22 @@ package WebBookStore.book;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-// Lombok 사용
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class bookVO {
-    private int id;             // 책 ID
-    private String title;       // 책 제목
-    private String author;      // 저자
-    private String publisher;   // 출판사
-    private double price;       // 가격
-    private int stock;          // 재고
+@Getter
+@Setter
+@ToString
+public class BookVO {
+    private int isbn;
+    private String bookname;
+    private String author;
+    private String publisher;
+    private String image;
+    private String price;
 }
