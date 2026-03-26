dfe59b9a431d933c4cfedf03819f7e129f1eba62
seoil <seoil@host.docker.internal>
2026-03-26 20:32:16 +0900

    0

--- WebBookStore/src/main/java/WebBookStore/book/UserBookController.java
+++ WebBookStore/src/main/java/WebBookStore/book/UserBookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/book")
+@RequestMapping("/")
 public class UserBookController {
 
 	@Autowired
@@ -18,7 +18,7 @@ public class UserBookController {
 	@RequestMapping("/list")
 	public String list(Model model) {
 		model.addAttribute("list", bookService.getBookList());
-		model.addAttribute("contentPage", "/WEB-INF/views/book/list.jsp");
+		model.addAttribute("contentPage", "/WEB-INF/views/user/views.jsp");
 		return "layout/layout";
 	}
 
@@ -26,7 +26,7 @@ public class UserBookController {
 	@RequestMapping("/view")
 	public String view(@RequestParam("isbn") int isbn, Model model) {
 		model.addAttribute("book", bookService.getBook(isbn));
-		model.addAttribute("contentPage", "/WEB-INF/views/book/view.jsp");
+		model.addAttribute("contentPage", "/WEB-INF/views/user/view.jsp");
 		return "layout/layout";
 	}
 }
