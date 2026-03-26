e59dab6df97968437e80b7e2f358f9239d8ca649
seoil <seoil@BY211-G-07>
2026-03-26 18:50:06 +0900

    insert

--- WebBookStore/src/main/java/WebBookStore/admin/AdminController.java
+++ WebBookStore/src/main/java/WebBookStore/admin/AdminController.java
@@ -21,43 +21,57 @@ public class AdminController {
 		model.addAttribute("contentPage", "index");
 		return "layout/layout";
 	}
+
+	// 등록폼 열기
+	@RequestMapping("/insertform")
+	public String insertform(Model model) {
+		String contentPage = "/WEB-INF/views/admin/insertform.jsp";
+		model.addAttribute("contentPage", contentPage);
+		return "layout/layout";
+	}
+
+	// 등록 처리
+	@RequestMapping("/insert")
+	public String insert(AdminVO admin, Model model, RedirectAttributes ra) {
+		System.out.println(admin);
+		ra.addFlashAttribute("kind", "insert");
+
+		if (adminService.insertBook(admin) > 0) {
+			ra.addFlashAttribute("message", "success");
+		} else {
+			ra.addFlashAttribute("message", "fail");
+		}
+
+		String contentPage = "/WEB-INF/views/user/views.jsp";
+		model.addAttribute("contentPage", contentPage);
+		return "layout/layout";
+	}
+
 	// 수정폼 열기
 	@RequestMapping("/updateform")
 	public ModelAndView updateform(int isbn, RedirectAttributes ra, HttpServletRequest request, Model model) {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("admin", adminService.getBookById(isbn));
-		
+
 		String contentPage = "/WEB-INF/views/admin/updateform.jsp";
-		//model.addAttribute("contentPage", contentPage);
 		mv.addObject("contentPage", contentPage);
-		
+
 		mv.setViewName("layout/layout");
 		return mv;
-		//return "layout/layout";
 	}
-	
+
 	// 수정 처리
 	@RequestMapping("/update")
-	public String update(AdminVO admin, Model model,
-		RedirectAttributes ra) {
-			System.out.println(admin);
-			ra.addFlashAttribute("kind","update");
-			if(adminService.updateBook(admin)) {
-				ra.addFlashAttribute("message","success"); 
-			}
-			else {
-				ra.addFlashAttribute("message","fail"); 
-			}
+	public String update(AdminVO admin, Model model, RedirectAttributes ra) {
+		System.out.println(admin);
+		ra.addFlashAttribute("kind", "update");
 
-			//return "redirect:/phonebook/view?id="+admin.getId();
-		/*	
-		boolean result = adminService.updateBook(admin);
-		if(result) {
-			
-		}else {
-			
+		if (adminService.updateBook(admin)) {
+			ra.addFlashAttribute("message", "success");
+		} else {
+			ra.addFlashAttribute("message", "fail");
 		}
-		*/
+
 		String contentPage = "/WEB-INF/views/user/views.jsp";
 		model.addAttribute("contentPage", contentPage);
 		return "layout/layout";
